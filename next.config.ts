011a11755401dd80a86da032218b3d8a8b9e1053
Anirudh <anirudhpatwal19@gmail.com>
2026-04-07 12:36:13 +0530

    simplified game

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@ const nextConfig: NextConfig = {
   transpilePackages: [
     "@initia/interwovenkit-react",
     "@initia/icons-react",
-    "@initia/utils"
-  ]
+    "@initia/utils",
+  ],
+  // Allow accessing the dev server from your LAN IP in development
+  allowedDevOrigins: ["192.168.1.9"],
 };
 
 export default nextConfig;
